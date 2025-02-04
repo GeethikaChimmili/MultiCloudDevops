58b996a116b8c35ff9089bf61b6d76107fd6e5a1
Geethika Chimmili <geethi.chimmili14@gmail.com>
2025-02-04 18:41:03 -0500

    modified app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from google.cloud import compute_v1
 
 def list_gcp_regions():
     client = compute_v1.RegionsClient()
-    project = "nareshit"  # Replace with your project ID
+    project = "NareshIT"  # Replace with your project ID
 
     request = compute_v1.ListRegionsRequest(project=project)
     response = client.list(request=request)
 
-    print("Available multi Cloud Regions:")
+    print("Available Google Cloud Regions:")
     for region in response:
         print(f"- {region.name} (Status: {region.status})")
 
